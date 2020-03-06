dd104db2a52255729e27835165c2d9912a9adac6
Ryan Zhang <yangzhangrice@hotmail.com>
2020-03-06 12:33:13 -0800

    initial check

---
+++ api/v1alpha2/containerizedworkload_types.go
@@ -0,0 +1,108 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha2
+
+import (
+	cpv1alpha1 "github.com/crossplaneio/crossplane-runtime/apis/core/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// An OperatingSystem required by a containerised workload.
+type OperatingSystem string
+
+// Supported operating system types.
+const (
+	OperatingSystemLinux   OperatingSystem = "linux"
+	OperatingSystemWindows OperatingSystem = "windows"
+)
+
+// A CPUArchitecture required by a containerised workload.
+type CPUArchitecture string
+
+// Supported architectures
+const (
+	CPUArchitectureI386  CPUArchitecture = "i386"
+	CPUArchitectureAMD64 CPUArchitecture = "amd64"
+	CPUArchitectureARM   CPUArchitecture = "arm"
+	CPUArchitectureARM64 CPUArchitecture = "arm64"
+)
+
+// A ContainerizedWorkloadSpec defines the desired state of a containerized Workload.
+type ContainerizedWorkloadSpec struct {
+	// OperatingSystem required by this workload.
+	// +kubebuilder:validation:Enum=linux;windows
+	// +optional
+	OperatingSystem *OperatingSystem `json:"osType,omitempty"`
+
+	// CPUArchitecture required by this workload.
+	// +kubebuilder:validation:Enum=i386;amd64;arm;arm64
+	// +optional
+	CPUArchitecture *CPUArchitecture `json:"arch,omitempty"`
+
+	// Containers of which this workload consists.
+	Containers []corev1.Container `json:"containers"`
+}
+
+// A ResourceReference refers to an resource managed by an OAM resource.
+type ResourceReference struct {
+	// APIVersion of the referenced resource.
+	APIVersion string `json:"apiVersion"`
+
+	// Kind of the referenced resource.
+	Kind string `json:"kind"`
+
+	// Name of the referenced resource.
+	Name string `json:"name"`
+
+	// UID of the referenced resource.
+	// +optional
+	UID *types.UID `json:"uid,omitempty"`
+}
+
+// A ContainerizedWorkloadStatus represents the observed state of a
+// ContainerizedWorkload.
+type ContainerizedWorkloadStatus struct {
+	cpv1alpha1.ConditionedStatus `json:",inline"`
+
+	// Resources managed by this containerised workload, key the resource UID
+	Resources map[types.UID]ResourceReference `json:"resources,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ContainerizedWorkload is the Schema for the containerizedworkloads API
+type ContainerizedWorkload struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ContainerizedWorkloadSpec   `json:"spec,omitempty"`
+	Status ContainerizedWorkloadStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ContainerizedWorkloadList contains a list of ContainerizedWorkload
+type ContainerizedWorkloadList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ContainerizedWorkload `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ContainerizedWorkload{}, &ContainerizedWorkloadList{})
+}
---
+++ controllers/containerizedworkload_controller_helper.go
@@ -0,0 +1,158 @@
+package controllers
+
+import (
+	"context"
+	"strings"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	oamv1alpha2 "github.com/oam-dev/core-resource-controller/api/v1alpha2"
+)
+
+const (
+	OAMResourceTypeLabel = "oam.dev/type"
+	OAMResourceNameLabel = "oam.dev/name"
+	KindDeployment       = "Deployment"
+	KindService          = "Service"
+)
+
+// OAMResourceTypes
+type OAMResourceTypes string
+
+// Supported OAM Resource Types
+const (
+	workloadType OAMResourceTypes = "workload"
+)
+
+// create a corresponding deployment
+func (r *ContainerizedWorkloadReconciler) renderWorkload(ctx context.Context,
+	workload *oamv1alpha2.ContainerizedWorkload) (*appsv1.Deployment, error) {
+	var RevisionHistoryLimit int32 = 100
+	deployName := workload.Name + "-deployment"
+	depl := appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: appsv1.SchemeGroupVersion.String(),
+			Kind:       KindDeployment,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deployName,
+			Namespace: workload.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{OAMResourceTypeLabel: string(workloadType), OAMResourceNameLabel: deployName},
+			},
+			RevisionHistoryLimit: &RevisionHistoryLimit,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{OAMResourceTypeLabel: string(workloadType), OAMResourceNameLabel: deployName},
+				},
+				Spec: corev1.PodSpec{
+					Containers: workload.Spec.Containers,
+				},
+			},
+		},
+	}
+
+	// always set the controller reference so that we know which object owns this.
+	if err := ctrl.SetControllerReference(workload, &depl, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return &depl, nil
+}
+
+// create a service for the deployment
+func (r *ContainerizedWorkloadReconciler) renderService(ctx context.Context, deploy *appsv1.Deployment,
+	port *corev1.ContainerPort) (*corev1.Service, error) {
+	svc := corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: corev1.SchemeGroupVersion.String(),
+			Kind:       KindService,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploy.Name + "-service",
+			Namespace: deploy.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:       strings.ToLower(string(port.Protocol)),
+					Port:       8080,
+					Protocol:   port.Protocol,
+					TargetPort: intstr.FromInt(int(port.ContainerPort)),
+				},
+			},
+			Selector: map[string]string{OAMResourceNameLabel: deploy.Name},
+			Type:     corev1.ServiceTypeNodePort,
+		},
+	}
+
+	// always set the controller reference so that we know which object owns this.
+	if err := ctrl.SetControllerReference(deploy, &svc, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return &svc, nil
+}
+
+// delete deployments/services that are not the same as the existing
+func (r *ContainerizedWorkloadReconciler) cleanupResources(ctx context.Context,
+	workload *oamv1alpha2.ContainerizedWorkload, deployUID, serviceUID *types.UID) error {
+	log := r.Log.WithValues("gc deployment", workload.Name)
+	if serviceUID == nil {
+		log.Info("Remove all services")
+	} else {
+		log.Info("Remove orphan services", "keep service UID", *serviceUID)
+	}
+	var deploy appsv1.Deployment
+	var service corev1.Service
+	removed := make([]types.UID, 0)
+	for uid, res := range workload.Status.Resources {
+		if res.Kind == KindDeployment {
+			if uid != *deployUID {
+				log.Info("Found an orphaned deployment", "deployment UID", *deployUID, "orphaned  UID", uid)
+				dn := client.ObjectKey{Name: res.Name, Namespace: workload.Namespace}
+				if err := r.Get(ctx, dn, &deploy); err != nil {
+					if apierrors.IsNotFound(err) {
+						continue
+					}
+					return err
+				}
+				if err := r.Delete(ctx, &deploy); err != nil {
+					return err
+				}
+				removed = append(removed, uid)
+				log.Info("Removed an orphaned deployment", "deployment UID", *deployUID, "orphaned UID", uid)
+			}
+		} else if res.Kind == KindService {
+			if serviceUID == nil || uid != *serviceUID {
+				log.Info("Found an orphaned service", "orphaned  UID", uid)
+				sn := client.ObjectKey{Name: res.Name, Namespace: workload.Namespace}
+				if err := r.Get(ctx, sn, &service); err != nil {
+					if apierrors.IsNotFound(err) {
+						continue
+					}
+					return err
+				}
+				if err := r.Delete(ctx, &service); err != nil {
+					return err
+				}
+				removed = append(removed, uid)
+				log.Info("Removed an orphaned service", "orphaned UID", uid)
+			}
+		}
+	}
+	// remove from status
+	for _, uid := range removed {
+		delete(workload.Status.Resources, uid)
+	}
+	return nil
+}
