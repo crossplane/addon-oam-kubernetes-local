515f2f2b0d2db9d03a0a0e66bd1d7f405d56b5ae
Ryan Zhang <yangzhangrice@hotmail.com>
2020-03-28 00:58:24 -0700

    fix the bugs

--- controllers/containerizedworkload_controller_helper.go
+++ controllers/containerizedworkload_controller_helper.go
@@ -2,14 +2,17 @@ package controllers
 
 import (
 	"context"
-
+	"fmt"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	oamv1alpha2 "github.com/crossplane/crossplane/apis/oam/v1alpha2"
+	wh "github.com/crossplane/crossplane/pkg/oam/workload"
+	cwh "github.com/crossplane/crossplane/pkg/oam/workload/containerized"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	oamv1alpha2 "github.com/crossplane/crossplane/apis/oam/v1alpha2"
 )
 
 const (
@@ -24,6 +27,61 @@ type OAMResourceTypes string
 const (
 	workloadType OAMResourceTypes = "workload"
 )
+
+// create a corresponding deployment
+func (r *ContainerizedWorkloadReconciler) renderDeployment(ctx context.Context,
+	workload *oamv1alpha2.ContainerizedWorkload) (*appsv1.Deployment, error) {
+
+	resources, err := cwh.Translator(ctx, workload)
+	if err != nil {
+		return nil, err
+	}
+	deploy, ok := resources[0].(*appsv1.Deployment)
+	if !ok {
+		return nil, fmt.Errorf("internal error, deployment is not rendered correctly")
+	}
+	// TODO: fix bug in the lib
+	r.Log.Info("translate a deployment", "deploy", deploy.Spec.Template.Spec.Containers[0])
+	deploy.Namespace = workload.Namespace
+
+	for i := 0; i < len(deploy.Spec.Template.Spec.Containers); i++ {
+		for j := 0; j < len(deploy.Spec.Template.Spec.Containers[i].Ports); j++ {
+			if len(deploy.Spec.Template.Spec.Containers[i].Ports[j].Protocol) == 0 {
+				deploy.Spec.Template.Spec.Containers[i].Ports[j].Protocol = corev1.ProtocolTCP
+			}
+		}
+	}
+	r.Log.Info(" rendered a deployment", "deploy", deploy.Spec.Template.Spec.Containers[0])
+	// always set the controller reference so that we can watch this deployment and it will be deleted automatically
+	if err := ctrl.SetControllerReference(workload, deploy, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return deploy, nil
+}
+
+// create a service for the deployment
+func (r *ContainerizedWorkloadReconciler) renderService(ctx context.Context,
+	workload *oamv1alpha2.ContainerizedWorkload, deploy *appsv1.Deployment) (*corev1.Service, error) {
+	// create a service for the workload
+	resources, err := wh.ServiceInjector(ctx, workload, []resource.Object{deploy})
+	if err != nil {
+		return nil, err
+	}
+	service, ok := resources[1].(*corev1.Service)
+	if !ok {
+		return nil, fmt.Errorf("internal error, service is not rendered correctly")
+	}
+	// TODO: fix it in the lib
+	service.Namespace = workload.Namespace
+	service.Spec.Ports[0].Protocol = corev1.ProtocolTCP
+
+	// always set the controller reference so that we can watch this service and
+	if err := ctrl.SetControllerReference(workload, service, r.Scheme); err != nil {
+		return nil, err
+	}
+	return service, nil
+}
 
 // delete deployments/services that are not the same as the existing
 func (r *ContainerizedWorkloadReconciler) cleanupResources(ctx context.Context,
